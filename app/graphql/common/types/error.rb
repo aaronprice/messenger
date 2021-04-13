97d0e8e5eeb8f6f06ea3a0d827671a78ffaafa93
Aaron Price <price.aaron@gmail.com>
2021-04-13 11:55:33 -0400

    Add formatting to GraphQL objects.

--- app/graphql/common/types/error.rb
+++ app/graphql/common/types/error.rb
@@ -1,10 +1,21 @@
 module Common
   module Types
     class Error < ::Types::BaseObject
+      
+      # == Definitions ==========================================================
+
       description "An error from a rails object"
+
+      # == Attributes ===========================================================
 
       field :field, String, null: false
       field :messages, [String], null: false
+
+      # == Relationships ========================================================
+
+      # == Class Methods ========================================================
+
+      # == Instance Methods =====================================================
     end
   end
 end
--- app/graphql/users/types/me.rb
+++ app/graphql/users/types/me.rb
@@ -1,11 +1,22 @@
 module Users
   module Types
     class Me < ::Types::BaseObject
+    
+      # == Definitions ==========================================================
+
       description "The current authenticated user."
+
+      # == Attributes ===========================================================
 
       field :id, ID, null: false
       field :name, String, null: false
       field :auth_token, String, null: false
+
+      # == Relationships ========================================================
+
+      # == Class Methods ========================================================
+
+      # == Instance Methods =====================================================
     end
   end
 end
